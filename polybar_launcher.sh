a2ae1c32e179a7971e58dac0434058bd51847260
zancas <zancas@zingolabs.org>
2024-09-10 17:12:48 -0600

    pass monitor, make width and offset explicit

--- polybar_launcher.sh
+++ polybar_launcher.sh
@@ -6,7 +6,10 @@ killall -q polybar
 # Wait until Polybar processes are fully terminated
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
+# Set the monitor environment variable
+MONITOR=eDP-1
+
 # Start Polybar for all your bars
-polybar -c ${HOME}/dotfiles/basic_polybar/polybar.config mainbar0 &
-polybar -c ${HOME}/dotfiles/basic_polybar/polybar.config mainbar1 &
-polybar -c ${HOME}/dotfiles/basic_polybar/polybar.config mainbar2 &
+monitor=$MONITOR polybar -c ${HOME}/dotfiles/basic_polybar/polybar.config mainbar0 &
+monitor=$MONITOR polybar -c ${HOME}/dotfiles/basic_polybar/polybar.config mainbar1 &
+monitor=$MONITOR polybar -c ${HOME}/dotfiles/basic_polybar/polybar.config mainbar2 &
